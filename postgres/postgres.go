652d1f44f1625bf71d8cdd70234dfdcb01f047cd
Kirill Belyaev <kirill10121415@gmail.com>
2026-06-06 14:17:23 +0300

    fix: make config all in strings

--- postgres/postgres.go
+++ postgres/postgres.go
@@ -5,7 +5,6 @@ import (
 	"context"
 	"fmt"
 	"net"
-	"strconv"
 
 	"github.com/jackc/pgx/v5/pgxpool"
 )
@@ -13,19 +12,19 @@ import (
 // Config описывает переменные и данные, необходимые для работы базой
 type Config struct {
 	Host     string `env:"HOST"`
-	Port     int    `env:"PORT"`
+	Port     string `env:"PORT"`
 	DB       string `env:"DB"`
 	Username string `env:"USER"`
 	Password string `env:"PASSWORD"`
-	MaxConns int    `env:"MAX_CONNS"`
-	MinConns int    `env:"MIN_CONNS"`
+	MaxConns string `env:"MAX_CONNS"`
+	MinConns string `env:"MIN_CONNS"`
 }
 
 // NewPool создает пул подлючений в бд
 func NewPool(ctx context.Context, cfg Config) (*pgxpool.Pool, error) {
 	// urlExample := "postgres://username:password@localhost:5432/database_name?sslmode=disable&pool_min_conns=%d&pool_max_conns=%d"
-	addr := net.JoinHostPort(cfg.Host, strconv.Itoa(cfg.Port))
-	connstring := fmt.Sprintf("postgres://%s:%s@%s/%s?sslmode=disable&pool_min_conns=%d&pool_max_conns=%d",
+	addr := net.JoinHostPort(cfg.Host, cfg.Port)
+	connstring := fmt.Sprintf("postgres://%s:%s@%s/%s?sslmode=disable&pool_min_conns=%s&pool_max_conns=%s",
 		cfg.Username,
 		cfg.Password,
 		addr,
